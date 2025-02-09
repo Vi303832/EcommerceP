aca42cb9ff7447653ede425a44921000ef199e27
Akif <mehmettanyeriakif3@hotmail.com>
2025-02-09 15:03:44 +0300

    Header

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Logo from "../Assets/Logo.jpg"
+import { FaSearch } from "react-icons/fa";
+import { IoCartSharp } from "react-icons/io5";
+import { FaUser } from "react-icons/fa";
+
+
 
 function Header() {
     return (
-        <div className='bg-ten h-[15vh] shadow-2xl'>
-            Header
+        <div className='bg-ten h-[15vh] shadow-2xl flex justify-between items-center px-5'>
+            <span><img className="size-21" src={Logo} /></span>
+            <span className='flex gap-3 items-center'>
+
+                <span className='flex items-center'>
+                    <FaSearch />
+                    <input className="border-2 box-border h-[90%]" type='text' />
+                </span>
+
+                <span className='flex items-center'><FaUser /></span>
+                <span className='flex items-center' ><IoCartSharp />0</span>
+
+            </span>
+
 
         </div>
     )
