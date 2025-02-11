d6661c2ca8fada51477c6a373af8ee9350989ce0
Akif <mehmettanyeriakif3@hotmail.com>
2025-02-11 14:03:41 +0300

    Filter Fonksiyonu eklendi

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -9,6 +9,8 @@ function Products() {
     let [products, setproducts] = useState([])
     let [plus, setplus] = useState("+")
     let [anim, setanim] = useState(false)
+    let [filter, setfilter] = useState(false)
+    let [selectedcat, setselectedcat] = useState([])
     let navigate = useNavigate();
 
     useEffect(() => {
@@ -49,25 +51,72 @@ function Products() {
         navigate(`/${nav}`)
 
     }
+    let handlefilter = () => {
+        setfilter(!filter)
+
+    }
 
 
     return (
         <div>
             <div className='min-h-screen w-[100%] font-arial text-bordo '>
-                <div className='p-15 cursor-pointer'>
-                    <span className='flex gap-2 items-center justfiy-center'><span className='text-5xl'>Bütün Ürünler</span><span onClick={Displayer} className={`text-5xl cursor-pointer transition transform ease-linear duration-200 ${plus === "-" ? 'rotate-180' : 'rotate-0'}`}>{plus}</span></span>
-                    <div
-                        className={`transition-all duration-1000 ease-in-out overflow-hidden ${anim ? "max-h-96 p-8 opacity-100" : "max-h-0 p-0 opacity-0"
-                            }`}
-                    >
-                        <div className="p-2 text-xl" onClick={() => handlenav("Hoodie")}>Hoodie</div>
-                        <div className="p-2 text-xl" onClick={() => handlenav("Sweatshirt")}>Sweatshirt</div>
-                        <div className="p-2 text-xl" onClick={() => handlenav("Tshirt")}>T-shirt</div>
-                        <div className="p-2 text-xl" onClick={() => handlenav("Other")}>Other Products</div>
+                <div className='flex justify-between'>
+                    <div className='p-15 cursor-pointer'>
+                        <span className='flex gap-2 items-center justfiy-center'><span className='text-5xl'>Bütün Ürünler</span><span onClick={Displayer} className={`text-5xl cursor-pointer transition transform ease-linear duration-200 ${plus === "-" ? 'rotate-180' : 'rotate-0'}`}>{plus}</span></span>
+                        <div
+                            className={`transition-all duration-1000 ease-in-out overflow-hidden ${anim ? "max-h-96 p-8 opacity-100" : "max-h-0 p-0 opacity-0"
+                                }`}
+                        >
+                            <div className="p-2 text-xl" onClick={() => handlenav("Hoodie")}>Hoodie</div>
+                            <div className="p-2 text-xl" onClick={() => handlenav("Sweatshirt")}>Sweatshirt</div>
+                            <div className="p-2 text-xl" onClick={() => handlenav("Tshirt")}>T-shirt</div>
+                            <div className="p-2 text-xl" onClick={() => handlenav("Other")}>Other Products</div>
+                        </div>
+
+
                     </div>
+                    <div className='p-15 cursor-pointer h-[100%]'>
+                        <span className='flex gap-2 items-center justfiy-center'><span className='text-xl px-3 py-1 rounded-2xl border-3' onClick={handlefilter}>Filter</span></span>
+                        <div
+                            className={`transition-all duration-1000 ease-in-out overflow-hidden bg-white rounded-3xl ${filter ? "max-h-96 py-8 opacity-100 px-32 " : "max-h-0 p-0 opacity-0"
+                                }`}
+                        >
+                            <div className='h-[100%]'>
+                                <div className='flex flex-row  justify-start'>
+                                    <div className='flex flex-col items-center justify-center w-[20vh] h-[60%]'>
+                                        <div className='text-2xl'>Modeller</div>
+                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Hoodie</div>
+                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Sweatshirt</div>
+                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >T-shirt</div>
+                                    </div>
+
+                                    <div className='flex flex-col items-center justify-center h-[60%]'>
+                                        <div className='text-2xl'>Renk</div>
+                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Saks Mavisi</div>
+                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Kahverengi</div>
+                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Gri</div>
+                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Ekru</div>
+                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Yeşil</div>
+                                    </div>
+
+
+                                </div>
+                            </div>
+
+
+                            <button>Uygula</button>
+                        </div>
+
+
+                    </div>
+
+
                 </div>
 
-                <div className=' bg-kahve transition-all duration-1000 ease-in-out mx-10 flex justify-center py-10 rounded-3xl'>
+
+
+                <div className=' bg-kahve transition-all duration-1000 ease-in-out mx-10 flex flex-col items-center justify-center py-10 rounded-3xl'>
+
 
                     <div className='grid grid-cols-3 mx-20 gap-x-10 gap-y-10 grid-rows-3 '>
 
