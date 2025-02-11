0befd468590b33723fb80cbe90206e627fe4792c
Akif <mehmettanyeriakif3@hotmail.com>
2025-02-11 18:06:02 +0300

    Searchbar-header-genel layout düzenlendi

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -3,47 +3,63 @@ import Logo from "../Assets/Logo.jpg"
 import { FaSearch } from "react-icons/fa";
 import { IoCartSharp } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
-import { useNavigate } from "react-router";
-
+import { useNavigate, } from "react-router";
+import { useState } from 'react';
 
 function Header() {
 
     let navigate = useNavigate();
+    let [open, setopen] = useState(false);
 
     const handleNavigate = (path) => {
         navigate(path);
     };
 
+    let handleopen = () => {
+
+        setopen(!open);
+
+    }
 
     return (
-        <div className=' h-[15vh] shadow-2xl flex justify-between items-center px-5 text-bordo font-arial'>
-            <span onClick={() => handleNavigate("/")}><img className="size-[10vh] cursor-pointer" src={Logo} /></span>
+        <div>
+            <div className=' h-[15vh] shadow-2xl grid grid-cols-3 grid-rows-1 px-5 text-bordo font-arial w-[100%]'>
+                <span className='flex justify-start items-center' onClick={() => handleNavigate("/")}><img className="size-[10vh] cursor-pointer" src={Logo} /></span>
 
-            <div className='flex gap-15 cursor-pointer '>
-                <div onClick={() => handleNavigate("/Ürünler")}>Ürünler</div>
-                <div>About Us</div>
-                <div>Contact</div>
-            </div>
-
-
-
+                <div className='flex gap-15 cursor-pointer items-center justify-around'>
+                    <div onClick={() => handleNavigate("/Ürünler")}>Ürünler</div>
+                    <div>About Us</div>
+                    <div>Contact</div>
+                </div>
 
 
 
-            <span className='flex gap-3 items-center'>
 
-                <span className='flex items-center gap-2'>
-                    <FaSearch />
-                    <input className="border-2 box-border h-[90%]" type='text' />
+
+
+                <span className='flex gap-3 items-center justify-end text-xl cursor-pointer '>
+
+                    <span className='flex items-center text-2xl'>
+                        <FaSearch onClick={() => handleopen()} />
+
+
+                    </span>
+
+                    <span className='flex items-center text-2xl'><FaUser /></span>
+                    <span className='flex items-center border-2  py-1 justify-between gap-1 rounded-3xl' ><span className='px-2 text-md flex gap-1 items-center'><IoCartSharp />Sepet</span><span className=' border-2 rounded-full text-center  px-2 mr-3 text-sm  '>0</span></span>
+
                 </span>
 
-                <span className='flex items-center'><FaUser /></span>
-                <span className='flex items-center' ><IoCartSharp />0</span>
 
-            </span>
+            </div>
+
+            {<input className={`shadow-sm box-border ease-in-out px-2 transition-all duration-1000 ${open ? "py-1  block w-[100%] bg-[#f7f6ec] h-16 " : "p-0 w-[100%] h-0  "}`} placeholder='İstediğiniz ürünü aratın , kolayca bulun!' type='text' />}
+
 
 
         </div>
+
+
     )
 }
 
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, query, where } from 'firebase/firestore'
 import { db } from '../Firebase'
 import { useNavigate } from 'react-router-dom'
 
@@ -11,6 +11,7 @@ function Products() {
     let [anim, setanim] = useState(false)
     let [filter, setfilter] = useState(false)
     let [selectedcat, setselectedcat] = useState([])
+
     let navigate = useNavigate();
 
     useEffect(() => {
@@ -55,6 +56,41 @@ function Products() {
         setfilter(!filter)
 
     }
+    let addclicked = (e) => {
+        if (selectedcat.includes(e.target.value)) {
+            setselectedcat(p => p.filter(a => a !== e.target.value));
+        }
+        else {
+            setselectedcat(p => [...p, e.target.value])
+        }
+
+
+    }
+    let getitems = async () => {
+
+        try {
+            if (!Array.isArray(selectedcat) || selectedcat.length === 0) {
+                setError('Please select at least one category or color.');
+                return;
+            }
+
+            const docRef = collection(db, "Ürünler");
+            let q = query(docRef, where("categorys", "array-contains-any", selectedcat));
+            let docsnap = await getDocs(q);
+
+            let items = [];
+            docsnap.forEach((doc) => {
+                items.push(doc.data());
+            });
+
+            setproducts(items);
+        } catch (error) {
+
+            console.error('Error getting documents: ', error);
+        } finally {
+
+        }
+    };
 
 
     return (
@@ -75,42 +111,113 @@ function Products() {
 
 
                     </div>
-                    <div className='p-15 cursor-pointer h-[100%]'>
-                        <span className='flex gap-2 items-center justfiy-center'><span className='text-xl px-3 py-1 rounded-2xl border-3' onClick={handlefilter}>Filter</span></span>
-                        <div
-                            className={`transition-all duration-1000 ease-in-out overflow-hidden bg-white rounded-3xl ${filter ? "max-h-96 py-8 opacity-100 px-32 " : "max-h-0 p-0 opacity-0"
+                    <div className='p-15 cursor-pointer h-[100%] '>
+                        <span className={'flex gap-2 items-center  justify-end p-2  '}>{<button onClick={() => setselectedcat([])}
+                            className={` text-xl  rounded-2xl border-3 transition-all ease-linear duration-1000 px-2 py-1 relative p-0 ${filter
+                                ? "opacity-100 left-0  "
+                                : "opacity-0  left-3 "
                                 }`}
                         >
-                            <div className='h-[100%]'>
-                                <div className='flex flex-row  justify-start'>
-                                    <div className='flex flex-col items-center justify-center w-[20vh] h-[60%]'>
-                                        <div className='text-2xl'>Modeller</div>
-                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Hoodie</div>
-                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Sweatshirt</div>
-                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >T-shirt</div>
+                            Clear
+                        </button>
+
+                        }<span className='text-xl px-3 py-1 rounded-2xl border-3' onClick={handlefilter}>Filter</span></span>
+                        <div
+                            className={`transition-all duration-1000 ease-in-out overflow-hidden bg-white rounded-3xl ${filter ? "max-h-[500px] py-8 opacity-100 px-32 " : "max-h-0 p-0 opacity-0"
+                                }`}
+                        >
+                            <div className='h-[100%] w-[100%]'>
+                                <div className="flex flex-row gap-10">
+                                    {/* Hoodie Selection */}
+                                    <div className="flex flex-col gap-2 items-center justify-center w-[20vh] h-[60%]">
+                                        <div className="text-2xl">Modeller</div>
+                                        <label className={` py-2 w-[20vh] text-center rounded-3xl text-lg box-border border-2 transition ease-linear duration-1000 ${selectedcat.includes("Hoodie") ? ' border-4 border-kahve scale-105' : 'scale-100'}`} >
+                                            <input
+                                                type="checkbox"
+                                                value="Hoodie"
+                                                className='hidden'
+                                                onClick={(e) => addclicked(e)}
+                                            />
+                                            Hoodie
+                                        </label>
+                                        <label className={`transition ease-linear duration-1000 py-2 w-[20vh] text-center rounded-3xl text-lg box-border border-2 ${selectedcat.includes("Sweatshirt") ? ' border-4 border-kahve scale-105' : 'scale-100'}`} >
+                                            <input
+                                                type="checkbox"
+                                                value="Sweatshirt"
+                                                onClick={(e) => addclicked(e)}
+                                                className='hidden'
+                                            />
+                                            Sweatshirt
+                                        </label>
+                                        <label className={`transition ease-linear duration-1000 py-2 w-[20vh] text-center rounded-3xl text-lg box-border border-2 ${selectedcat.includes("Tshirt") ? ' border-4 border-kahve scale-105' : 'scale-100'}`} >
+                                            <input
+                                                type="checkbox"
+                                                value="Tshirt"
+                                                className='hidden'
+                                                onClick={(e) => addclicked(e)}
+                                            />
+                                            T-shirt
+                                        </label>
                                     </div>
 
-                                    <div className='flex flex-col items-center justify-center h-[60%]'>
-                                        <div className='text-2xl'>Renk</div>
-                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Saks Mavisi</div>
-                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Kahverengi</div>
-                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Gri</div>
-                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Ekru</div>
-                                        <div className="py-2 w-[20vh] text-center rounded-3xl text-lg border-2" >Yeşil</div>
+                                    {/* Color Selection */}
+                                    <div className="flex flex-col items-center justify-center  h-[60%] gap-2 ">
+                                        <div className="text-2xl">Renk</div>
+                                        <label className={`transition ease-linear duration-1000 py-2 w-[20vh] text-center rounded-3xl text-lg box-border border-2 ${selectedcat.includes("Saks Mavisi") ? ' border-4 border-kahve scale-105' : 'scale-100'}`} >
+                                            <input
+                                                type="checkbox"
+                                                value="Saks Mavisi"
+                                                className='hidden'
+                                                onClick={(e) => addclicked(e)}
+                                            />
+                                            Saks Mavisi
+                                        </label>
+                                        <label className={`py-2 w-[20vh] text-center rounded-3xl transition ease-linear duration-1000 text-lg box-border border-2 ${selectedcat.includes("Kahverengi") ? ' border-4 border-kahve scale-105' : 'scale-100'}`} >
+                                            <input
+                                                type="checkbox"
+                                                value="Kahverengi"
+                                                className='hidden'
+                                                onClick={(e) => addclicked(e)}
+                                            />
+                                            Kahverengi
+                                        </label>
+                                        <label className={`transition ease-linear duration-1000 py-2 w-[20vh] text-center rounded-3xl text-lg box-border border-2 ${selectedcat.includes("Gri") ? ' border-4 border-kahve scale-105' : 'scale-100'}`} >
+                                            <input
+                                                type="checkbox"
+                                                value="Gri"
+                                                className='hidden'
+                                                onClick={(e) => addclicked(e)}
+                                            />
+                                            Gri
+                                        </label>
+                                        <label className={`py-2 w-[20vh] text-center transition ease-linear duration-1000 rounded-3xl text-lg box-border border-2 ${selectedcat.includes("Ekru") ? ' border-4 border-kahve scale-105' : 'scale-100'}`} >
+                                            <input
+                                                type="checkbox"
+                                                value="Ekru"
+                                                className='hidden'
+                                                onClick={(e) => addclicked(e)}
+                                            />
+                                            Ekru
+                                        </label>
+                                        <label className={`transition ease-linear duration-1000 py-2 w-[20vh] text-center rounded-3xl text-lg box-border border-2 ${selectedcat.includes("Yeşil") ? ' border-4 border-kahve scale-105' : 'scale-100'}`} >
+                                            <input
+                                                type="checkbox"
+                                                value="Yeşil"
+                                                className='hidden'
+                                                onClick={(e) => addclicked(e)}
+                                            />
+                                            Yeşil
+                                        </label>
                                     </div>
-
-
                                 </div>
+
+
                             </div>
+                            <button onClick={getitems} className='py-2 w-[20vh] text-center rounded-3xl text-lg box-border border-2'>Uygula</button>
 
-
-                            <button>Uygula</button>
                         </div>
 
-
                     </div>
-
-
                 </div>
 
 
